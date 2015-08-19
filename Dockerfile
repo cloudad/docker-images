5afd69cbbf83a9168adaed2f10393f410854efbe
Piotr Minkina <p.minkina@piotrminkina.pl>
2015-08-19 07:38:36 +0200

    [BUILD] Implemented command shell, preparing image in automatic manner and allowed override project env parameters

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,22 @@
 FROM ubuntu-debootstrap:vivid
 
 
-ENV PROJECT_DIR=/usr/src/docker-images
-ENV SOURCE_DIR=${PROJECT_DIR}/src
-ENV BUILD_DIR=${PROJECT_DIR}/build
-
 RUN apt-get update \
  && DEBIAN_FRONTEND=noninteractive apt-get install -yqq \
     software-properties-common \
- && find /var/cache/apt/archives/ /var/lib/apt/lists/ -type f -exec rm {} +
+ && find \
+    /var/cache/apt/archives/ \
+    /var/lib/apt/lists/ \
+    -type f \
+    -exec rm {} +
 
 RUN apt-add-repository ppa:ansible/ansible \
  && apt-get update \
  && DEBIAN_FRONTEND=noninteractive apt-get install -yqq \
     ansible \
     debootstrap \
- && find /var/cache/apt/archives/ /var/lib/apt/lists/ -type f -exec rm {} +
-
-VOLUME ${PROJECT_DIR}/
-WORKDIR ${SOURCE_DIR}/
-ENTRYPOINT [ \
-    "ansible-playbook", \
-    "--inventory-file=suites", \
-    "site.yml" \
-]
-CMD []
+ && find \
+    /var/cache/apt/archives/ \
+    /var/lib/apt/lists/ \
+    -type f \
+    -exec rm {} +
--- docker-build.sh
+++ docker-build.sh
@@ -1,55 +1,94 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 
-declare IMAGE_NAME='docker-images'
-declare CONTAINER_NAME='docker-images'
-declare BUILD_PATH='/usr/src/docker-images'
-declare ARG
+PROJECT_NAME="$(readlink -f $0|xargs dirname|xargs basename)"
+PROJECT_PATH="$(readlink -f $0|xargs dirname)"
+
+IMAGE_NAME="${IMAGE_NAME:-${PROJECT_NAME}}"
+CONTAINER_NAME="${CONTAINER_NAME:-${PROJECT_NAME}}"
+
+SOURCE_DIR="${SOURCE_DIR:-src}"
+BUILD_DIR="${BUILD_DIR:-build}"
+
+SOURCE_PATH="${PROJECT_PATH}/${SOURCE_DIR}"
+BUILD_PATH="${PROJECT_PATH}/${BUILD_DIR}"
+BUILD_ENTRYPOINT="ansible-playbook site.yml -i suites -e build_path='${BUILD_PATH}'"
+SHELL_ENTRYPOINT="bash"
 
 
 docker_prepare() {
-    docker build --pull -t "${IMAGE_NAME}" .
+    if [ -z "$(docker images -q "${IMAGE_NAME}")" ]; then
+        docker build --pull -t "${IMAGE_NAME}" .
+    fi
+
+    if [ -n "$(docker ps -q --filter "name=${CONTAINER_NAME}\$")" ]; then
+        printf 'Container "%s" is currently working...\n' "${CONTAINER_NAME}" 1>&2
+        exit 1
+    fi
+
+    if [ -n "$(docker ps -aq --filter "name=${CONTAINER_NAME}\$")" ]; then
+        docker rm -vf "${CONTAINER_NAME}"
+    fi
 }
 
 docker_build() {
+    docker_prepare
+
     docker run \
         --name "${CONTAINER_NAME}" \
+        --entrypoint="${BUILD_ENTRYPOINT}" \
         --privileged \
         --rm \
         -t \
-        -v "$(readlink -f $0|xargs dirname):${BUILD_PATH}" \
+        -v "${PROJECT_PATH}:${PROJECT_PATH}" \
+        -w "${SOURCE_PATH}" \
+        "${CONTAINER_NAME}" \
+        "$@"
+}
+
+docker_shell() {
+    docker_prepare
+
+    docker run \
+        --name "${CONTAINER_NAME}" \
+        --entrypoint "${SHELL_ENTRYPOINT}" \
+        --privileged \
+        --rm \
+        -it \
+        -v "${PROJECT_PATH}:${PROJECT_PATH}" \
+        -w "${SOURCE_PATH}" \
         "${CONTAINER_NAME}" \
         "$@"
 }
 
 docker_clean() {
-    docker rm -f -v "${CONTAINER_NAME}"
+    docker rm -fv "${CONTAINER_NAME}"
     docker rmi -f "${IMAGE_NAME}"
 }
 
 usage() {
-    echo "Usage: $0 [command] [...]" 1>&2
+    printf 'Usage: %s [command] [...]\n' "$0"
     exit 1
 }
 
 main() {
     while [ "$#" -gt 0 ]; do
         case "$1" in
-            prepare)
-                echo '>>> prepare:'
-                docker_prepare
-                ;;
             build)
                 echo '>>> build:'
                 docker_build
+                ;;
+            shell)
+                echo '>>> shell:'
+                docker_shell
                 ;;
             clean)
                 echo '>>> clean:'
                 docker_clean
                 ;;
             *)
-                echo 'Unknown command "'$1'"' 1>&2
+                printf 'Unknown command "%s"\n' "$1" 1>&2
                 usage
                 ;;
         esac
